48903be1fede7141b71dbcc7570838ce92435610
Artur Kaceli <81331837+Rutra-beep@users.noreply.github.com>
2022-11-24 19:53:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let btn = document.querySelector('.nav-menu')
 let navbar = document.querySelector('.mobile-nav')
+let mobile_socials = document.querySelectorAll('.mobile-social')
+let mobile_links = document.querySelectorAll('.mobile-links')
 
 btn.addEventListener('click', ()=>{
     navbar.classList.toggle('active')
 })
+
+for (var i = 0; i < mobile_socials.length;i++){
+    mobile_socials.addEventListener('click', ()=>{
+        navbar.classList.toggle('active');
+    })
+}
