0d3b6dc102cc79099988ed3ba420ec7759cd7877
Artur Kaceli <arturkaceli15@gmail.com>
2022-11-25 20:41:46 +0100

    asas

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let btn = document.querySelector('.nav-menu')
 let navbar = document.querySelector('.mobile-nav')
-let mobile_socials = document.querySelectorAll('.mobile-social')
-let mobile_links = document.querySelectorAll('.mobile-links')
 
 btn.addEventListener('click', ()=>{
     navbar.classList.toggle('active')
@@ -9,6 +7,11 @@ btn.addEventListener('click', ()=>{
 
 for (var i = 0; i < mobile_socials.length;i++){
     mobile_socials.addEventListener('click', ()=>{
+        navbar.classList.toggle('active');
+    })
+}
+for (var i = 0; i < mobile_links.length;i++){
+    mobile_links.addEventListener('click', ()=>{
         navbar.classList.toggle('active');
     })
 }
