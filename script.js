c73db5c459803a0a6267096b458a9f8a32d8d926
Artur Kaceli <arturkaceli15@gmail.com>
2022-12-06 22:40:56 +0100

    website updated

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let btn = document.querySelector('.nav-menu')
 let navbar = document.querySelector('.mobile-nav')
+let mobile_socials = document.querySelectorAll('.mobile-social')
+mobile_links = document.querySelectorAll('.mobile-link')
 
+console.log(mobile_links);
 btn.addEventListener('click', ()=>{
     navbar.classList.toggle('active')
 })
 
-for (var i = 0; i < mobile_socials.length;i++){
-    mobile_socials.addEventListener('click', ()=>{
-        navbar.classList.toggle('active');
+for (var i = 0, len = mobile_links.length; i < len; i++) {
+    mobile_links[i].addEventListener('click', ()=>{
+       navbar.classList.toggle('active');
     })
 }
-for (var i = 0; i < mobile_links.length;i++){
-    mobile_links.addEventListener('click', ()=>{
-        navbar.classList.toggle('active');
+for (var i = 0, len = mobile_socials.length; i < len; i++) {
+    mobile_socials[i].addEventListener('click', ()=>{
+       navbar.classList.toggle('active')
     })
 }
